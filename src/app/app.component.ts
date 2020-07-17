38e5020ffc139a60d02c70d09edf8d74c1cef6c5
@wendellmva <wmuntslag@live.nl>
2020-07-17 15:50:33 +0300

    feat(#1): create angular app $ ng new serastrip --prefix llt

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'llt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'serastrip';
+}
